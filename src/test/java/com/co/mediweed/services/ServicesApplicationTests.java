68c5d9c2475aecfd8e5893d0678a86d226ac0092
Camilo Medina <camilo.rivera@sensedia.com>
2022-08-25 09:12:36 -0500

    base line

---
+++ src/test/java/com/co/mediweed/services/ServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.mediweed.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
